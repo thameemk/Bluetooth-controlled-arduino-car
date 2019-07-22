840def2c2fb35009143bffa0debab3facd76f8e4
thameemk612 <thameemk612@gmail.com>
2019-07-22 21:01:28 +0530

    setup function

--- Drive.ino
+++ Drive.ino
@@ -1,6 +1,14 @@
 char data=0;  //variable for storing recieved data
 
-#define C1A 13  //right side
+#define C1A 13  //right side 
 #define C1B 7
 #define C2A 5  //left side
 #define C2B 8
+
+void setup(){
+    Serial.begin(9600);
+    pinMode(C1A,OUTPUT);
+    pinMode(C1B,OUTPUT);
+    pinMode(C2A,OUTPUT);
+    pinMode(C2B,OUTPUT);
+}
